a0ca62c8d3568ce66a2d072e1c33708dec4b09b6
Kyaw-Zin-George <ktz1695@gmail.com>
2024-09-02 10:36:34 +0100

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Kyaw Thant Zin(George) on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
